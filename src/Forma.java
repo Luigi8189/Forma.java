b332f0ec27dc16bf1897b311e2fe1d17305d3451
Luigi Campana <luigi8939@gmail.com>
2025-07-22 13:36:09 +0200

    .

--- src/Forma.java
+++ src/Forma.java
@@ -1,5 +1,5 @@
 public class Forma {
     public void calcolaArea() {
-        System.out.println("Area della forma generica.");
+        System.out.println("Area della forma generica.  ");
     }
 }
